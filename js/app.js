8e9387a5a9d728a74a44bf75de28281e047c38c7
Victor Aranguren <victoraranguren.dev@gmail.com>
2021-01-17 21:05:05 -0400

    Funcion showMessages() creada

--- js/app.js
+++ js/app.js
@@ -4,7 +4,21 @@ const resultado = document.querySelector('#resultado')
 
 const verification = (e)=>{
   e.preventDefault()
-  return input.value ? console.log(`El valor existe y es: ${input.value}`) : resultado.innerHTML = 'Está Vacío' 
+  return input.value ? console.log(`El valor existe y es: ${input.value}`) : showMessages('Está Vacío') 
+}
+
+const showMessages =(msg)=>{
+  const alert= document.createElement('div');
+
+  alert.classList.add('bg-bray-100', 'p-3', 'mt-3', 'text-center', 'alert');
+
+  alert.textContent = msg;
+
+  form.appendChild(alert)
+
+  setTimeout(() => {
+    alert.remove()
+  }, 3000);
 }
 
 document.addEventListener("DOMContentLoaded", () => {
