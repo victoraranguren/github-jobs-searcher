bad08fa3b8d72f3d0d5e5575c83d0ffc776ae1f8
Victor Aranguren <victoraranguren.dev@gmail.com>
2021-01-17 20:59:24 -0400

    Estilos de background cambiados otra vez...

--- js/app.js
+++ js/app.js
@@ -1,9 +1,10 @@
 const form = document.querySelector("#form");
 const input = document.querySelector('#input')
+const resultado = document.querySelector('#resultado')
 
 const verification = (e)=>{
   e.preventDefault()
-  return input.value ? console.log(`El valor existe y es: ${input.value}`) : console.log('Undefined') 
+  return input.value ? console.log(`El valor existe y es: ${input.value}`) : resultado.innerHTML = 'Está Vacío' 
 }
 
 document.addEventListener("DOMContentLoaded", () => {
